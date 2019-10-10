8690322f2a746b085da593bcae45fc2a30db0015
nubyia <shareenca@gmail.com>
2019-10-10 12:53:14 -0400

    Ex502

---
+++ src/main/java/com/example/demo/Sb502Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb502Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb502Application.class, args);
+    }
+
+}
